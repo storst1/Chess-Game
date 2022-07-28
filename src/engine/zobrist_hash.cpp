3eec91784a95726c9408192085cbf9b8ced6dcd2
storst1 <89082426+storst1@users.noreply.github.com>
2022-07-28 11:49:14 +0300

    Adding access methods to Zobrist hash class

--- src/engine/zobrist_hash.cpp
+++ src/engine/zobrist_hash.cpp
@@ -1,6 +1,6 @@
 #include "zobrist_hash.h"
 
-//Creates uninitialized object that is not ready to be used properly
+//Creates uninitialized object that is NOT ready to be used properly
 Zobrist_Hash::Zobrist_Hash()
 {
 
@@ -10,8 +10,10 @@ Zobrist_Hash::Zobrist_Hash()
 void Zobrist_Hash::Init() noexcept
 {
     for(int i = 0; i < 12; ++i){
-        for(int j = 0; j < 64; ++j){
-            pieces_hash[i][j] = QRandomGenerator::global()->generate64();
+        for(int j = 0; j < 8; ++j){
+            for(int g = 0; g < 8; ++g){
+                pieces_hash[i][j][g] = QRandomGenerator::global()->generate64();
+            }
         }
     }
     for(int i = 0; i < 8; ++i){
@@ -24,14 +26,35 @@ void Zobrist_Hash::Init() noexcept
     black_castle_queenside = QRandomGenerator::global()->generate64();
 }
 
-/*
-constexpr uint_fast64_t Zobrist_Hash::UpperBound() noexcept
+uint_fast64_t &Zobrist_Hash::Piece(int_fast8_t piece, uint_fast8_t x, uint_fast8_t y) noexcept
 {
-    return UINT64_MAX;
+    if(piece < 0){
+        piece += 13;
+    }
+    return pieces_hash[piece - 1][x][y];
 }
 
-constexpr uint_fast64_t Zobrist_Hash::LowerBound() noexcept
+uint_fast64_t &Zobrist_Hash::Move() noexcept
 {
-    return 100;
+    return black_move;
 }
-*/
+
+uint_fast64_t &Zobrist_Hash::WCK() noexcept
+{
+    return white_castle_kingside;
+}
+
+uint_fast64_t &Zobrist_Hash::WCQ() noexcept
+{
+    return white_castle_queenside;
+}
+
+uint_fast64_t &Zobrist_Hash::BCK() noexcept
+{
+    return black_castle_kingside;
+}
+
+uint_fast64_t &Zobrist_Hash::BCQ() noexcept
+{
+    return black_castle_queenside;
+}
--- src/engine/zobrist_hash.h
+++ src/engine/zobrist_hash.h
@@ -13,10 +13,14 @@ public:
 
     void Init() noexcept;
 
-    //static constexpr uint_fast64_t UpperBound() noexcept;
-    //static constexpr uint_fast64_t LowerBound() noexcept;
+    uint_fast64_t& Piece(int_fast8_t piece, uint_fast8_t x, uint_fast8_t y) noexcept;
+    uint_fast64_t& Move() noexcept;
+    uint_fast64_t& WCK() noexcept;
+    uint_fast64_t& WCQ() noexcept;
+    uint_fast64_t& BCK() noexcept;
+    uint_fast64_t& BCQ() noexcept;
 private:
-    uint_fast64_t pieces_hash[12][64];
+    uint_fast64_t pieces_hash[12][8][8];
     uint_fast64_t black_move;
     uint_fast64_t en_passant_files[8];
     uint_fast64_t white_castle_kingside;

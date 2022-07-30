b5909ec362eb08569e665b0d4f07176750954e74
storst1 <89082426+storst1@users.noreply.github.com>
2022-07-30 22:47:22 +0300

    Removing unnecessary debug lines from main.cpp

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,13 +1,11 @@
 #include "window/window.h"
 
 #include <QApplication>
-#include <QDir>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    qDebug() << QDir::currentPath();
     w.show();
     return a.exec();
 }
